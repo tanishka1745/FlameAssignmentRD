779de2715542b4216b6705c1d47592afb0ac3d09
Tanishka <tanishkasharma1745@gmail.com>
2025-10-04 21:49:42 +0530

    added: the native bridge for communicate with native code.

--- app/src/main/java/com/example/flameassignmentrd/app/NativeBridge.kt
+++ app/src/main/java/com/example/flameassignmentrd/app/NativeBridge.kt
@@ -1,5 +1,9 @@
 package com.example.flameassignmentrd.app
 
+
+
+// Singleton class for initiate one time object
+
 object NativeBridge {
     init { System.loadLibrary("native-lib") }
     external fun processNV21(nv21: ByteArray, width: Int, height: Int): ByteArray
