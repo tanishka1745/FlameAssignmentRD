5a9f107119e29db2970826876ecbe15bb451b167
Tanishka <tanishkasharma1745@gmail.com>
2025-10-04 18:09:00 +0530

    chores: added function nv21 and canny detection

--- app/src/main/jni/native-lib.cpp
+++ app/src/main/jni/native-lib.cpp
@@ -2,23 +2,25 @@
 #include <opencv2/opencv.hpp>
 using namespace cv;
 
-// Helper functions
+
+// Converts NV21 camera byte array to an OpenCV BGR Mat
 Mat nv21ToMat(jbyte* nv21, int width, int height) {
     Mat yuv(height + height / 2, width, CV_8UC1, (unsigned char*)nv21);
     Mat bgr;
     cvtColor(yuv, bgr, COLOR_YUV2BGR_NV21);
     return bgr;
 }
-
+// Applies Canny edge detection on an input BGR image
 Mat applyCanny(Mat &input) {
     Mat gray, edges;
     cvtColor(input, gray, COLOR_BGR2GRAY);
+    // Apply Canny edge detection
+    // 100 = lower threshold, 200 = upper threshold
     Canny(gray, edges, 100, 200);
     cvtColor(edges, edges, COLOR_GRAY2BGR);
     return edges;
 }
 
-// Correct JNI linkage for package com.example.flameassignmentrd.app
 extern "C" JNIEXPORT jbyteArray JNICALL
 Java_com_example_flameassignmentrd_app_NativeBridge_processNV21(
         JNIEnv *env,
@@ -43,6 +45,5 @@ Java_com_example_flameassignmentrd_app_NativeBridge_processNV21(
 
     // Release input array
     env->ReleaseByteArrayElements(nv21_, nv21, 0);
-
     return out;
 }
