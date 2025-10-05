c71f6017ffc5010c8e7194acf311d8376b4e8728
Tanishka <tanishkasharma1745@gmail.com>
2025-10-05 14:32:46 +0530

    fix: added STL c++ library

--- app/src/main/java/com/example/flameassignmentrd/app/NativeBridge.kt
+++ app/src/main/java/com/example/flameassignmentrd/app/NativeBridge.kt
@@ -5,6 +5,9 @@ package com.example.flameassignmentrd.app
 // Singleton class for initiate one time object
 
 object NativeBridge {
-    init { System.loadLibrary("native-lib") }
+    init {
+        System.loadLibrary("c++_shared")
+        System.loadLibrary("opencv_java4")
+        System.loadLibrary("native-lib") }
     external fun processNV21(nv21: ByteArray, width: Int, height: Int): ByteArray
 }
